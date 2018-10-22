2894dea7e81119f0ed11de567a4cc5be304e9d21
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2018-10-22 12:06:09 +0200

    Initial Commit

---
+++ HapticFeedbackExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HapticFeedbackExample
+//
+//  Created by Giuseppe Travasoni on 22/10/2018.
+//  Copyright © 2018 Travasoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
