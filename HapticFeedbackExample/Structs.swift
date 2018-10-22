4df7feda4a5bf4b6db0cb979a50c2f8aa39e291f
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2018-10-22 15:53:00 +0200

    implementation

---
+++ HapticFeedbackExample/Structs.swift
@@ -0,0 +1,94 @@
+//
+//  Structs.swift
+//  HapticFeedbackExample
+//
+//  Created by Giuseppe Travasoni on 22/10/2018.
+//  Copyright © 2018 Travasoni. All rights reserved.
+//
+
+import UIKit
+
+protocol HapticStruct {
+    
+    var sectionTitle: String { get }
+    var numberOfCells: Int { get }
+    var cellTitles: [String] { get }
+    func action(for index: Int)
+}
+
+struct UIImpactFeedbackGeneratorStruct: HapticStruct {
+    
+    var sectionTitle: String {
+        return "UIImpactFeedbackGenerator"
+    }
+    
+    var numberOfCells: Int {
+        return 3
+    }
+    
+    var cellTitles: [String] {
+        return ["Syle Light", "Style Medium", "Style Heavy"]
+    }
+    
+    func action(for index: Int) {
+        switch index {
+        case 0:
+            UIImpactFeedbackGenerator(style: .light).impactOccurred()
+        case 1:
+            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+        case 2:
+            UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+        default:
+            return
+        }
+    }
+}
+
+struct UINotificationFeedbackGeneratorStruct: HapticStruct {
+    
+    var sectionTitle: String {
+        return "UINotificationFeedbackGenerator"
+    }
+    
+    var numberOfCells: Int {
+        return 3
+    }
+    
+    var cellTitles: [String] {
+        return ["Notification type Error", "Notification type Success", "Notification type Warning"]
+    }
+    
+    func action(for index: Int) {
+        switch index {
+        case 0:
+            UINotificationFeedbackGenerator().notificationOccurred(.error)
+        case 1:
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+        case 2:
+            UINotificationFeedbackGenerator().notificationOccurred(.warning)
+        default:
+            return
+        }
+    }
+}
+
+struct UISelectionFeedbackGeneratorStruct: HapticStruct {
+    
+    var sectionTitle: String {
+        return "UISelectionFeedbackGenerator"
+    }
+    
+    var numberOfCells: Int {
+        return 1
+    }
+    
+    var cellTitles: [String] {
+        return ["Selection Changed"]
+    }
+    
+    func action(for index: Int) {
+        UISelectionFeedbackGenerator.init().selectionChanged()
+    }
+    
+    
+}
--- HapticFeedbackExample/ViewController.swift
+++ HapticFeedbackExample/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    let hapticStructs: [HapticStruct] = [UIImpactFeedbackGeneratorStruct(), UINotificationFeedbackGeneratorStruct(), UISelectionFeedbackGeneratorStruct()]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Haptic Feedbacks"
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard hapticStructs.indices.contains(section) else {
+            return nil
+        }
+        return hapticStructs[section].sectionTitle
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard hapticStructs.indices.contains(section) else {
+            return 0
+        }
+        return hapticStructs[section].numberOfCells
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        guard hapticStructs.indices.contains(indexPath.section),
+            hapticStructs[indexPath.section].cellTitles.indices.contains(indexPath.row) else {
+            return cell
+        }
+        cell.textLabel?.text = hapticStructs[indexPath.section].cellTitles[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard hapticStructs.indices.contains(indexPath.section) else {
+            return
+        }
+        hapticStructs[indexPath.section].action(for: indexPath.row)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
